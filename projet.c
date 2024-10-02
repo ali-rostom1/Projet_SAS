ff23bc4510f178fd36ffcdfe86588383853bce2a
michael-rostom <ali.rostom220@gmail.com>
2024-10-02 17:27:02 +0100

    second commit

---
+++ projet.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#define MAX_RESERVATIONS 100
+#define MAX_LENGTH 50
+
+
+typedef struct{             //define a substructure 
+    int day;
+    int month;
+    int year;
+}Date;
+typedef enum{               //define an enum for status because it contains 4 constants
+    valid,
+    reported,
+    canceled,
+    treated
+}Status;
+
+typedef struct{                 //this is the main reservation structure
+    char *firstName;
+    char *lastName;         
+    char *phone;
+    char *age;
+    Status status;
+    int id;
+    Date date;
+}Reservation;
+
